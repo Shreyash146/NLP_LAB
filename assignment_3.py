52fb23dc8d0f47fecabe258997e6c8cf98de2470
Shreyash146 <113356967+Shreyash146@users.noreply.github.com>
2023-11-30 22:44:15 +0530

    Update assignment_3.py

--- assignment_3.py
+++ assignment_3.py
@@ -1,253 +1,66 @@
-'''Name: Shreyash Devanand Jeughale 
-Batch:B2
-Roll no:30
-Pract no 3: Generating the n gram model using nltk'''
-from nltk import ngrams
-
-from nltk.util import ngrams
-#unigram model
-print("------Unigram Model------")
-n = 1
-sentence = 'The story follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, where he explores the Grand Line in search of the mythical treasure known as the "One Piece" in order to become the next King of the Pirates.'
-unigrams = ngrams(sentence.split(), n)
-
-for item in unigrams:
-    print(item)
-print("\n\n")
-#bigram model
-print("------Bigram Model------")
-n = 2
-sentence = 'The story follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, where he explores the Grand Line in search of the mythical treasure known as the "One Piece" in order to become the next King of the Pirates.'
-unigrams = ngrams(sentence.split(), n)
-
-for item in unigrams:
-    print(item)
-print("\n\n")
-print("------Trigram Model------")
-#trigram model
-n = 3
-sentence = 'The story follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, where he explores the Grand Line in search of the mythical treasure known as the "One Piece" in order to become the next King of the Pirates.'
-unigrams = ngrams(sentence.split(), n)
-
-for item in unigrams:
-    print(item)
-
-print("\n\n")
-#using text file input
-print("------Trigram Model using text file------")
-from nltk import ngrams
-file = open("sampletext_2.txt")
-for i in file.readlines():
-    cumulative = i
-    sentences = i.split(".")
-    counter = 0
-    for sentence in sentences:
-        trigrams = ngrams(sentence.split(" "), 3)
-        print("For sentence", counter + 1, ", trigrams are: ")
-        for grams in trigrams:
-            print(grams)
-        counter += 1
-        print()
-
- #output    
 '''
-------Unigram Model------
-('The',)
-('story',)
-('follows',)
-('the',)
-('adventures',)
-('of',)
-('Monkey',)
-('D.',)
-('Luffy',)
-('and',)
-('his',)
-('crew,',)
-('the',)
-('Straw',)
-('Hat',)
-('Pirates,',)
-('where',)
-('he',)
-('explores',)
-('the',)
-('Grand',)
-('Line',)
-('in',)
-('search',)
-('of',)
-('the',)
-('mythical',)
-('treasure',)
-('known',)
-('as',)
-('the',)
-('"One',)
-('Piece"',)
-('in',)
-('order',)
-('to',)
-('become',)
-('the',)
-('next',)
-('King',)
-('of',)
-('the',)
-('Pirates.',)
+Assignment No: 3
+Name: Shreyash Jeughale 
+Roll: 30
+Batch: B2
+Title: Implements Named Entity Recognition(NER) on textual data using SpaCy or NLTK library.
+'''
 
 
 
-------Bigram Model------
-('The', 'story')
-('story', 'follows')
-('follows', 'the')
-('the', 'adventures')
-('adventures', 'of')
-('of', 'Monkey')
-('Monkey', 'D.')
-('D.', 'Luffy')
-('Luffy', 'and')
-('and', 'his')
-('his', 'crew,')
-('crew,', 'the')
-('the', 'Straw')
-('Straw', 'Hat')
-('Hat', 'Pirates,')
-('Pirates,', 'where')
-('where', 'he')
-('he', 'explores')
-('explores', 'the')
-('the', 'Grand')
-('Grand', 'Line')
-('Line', 'in')
-('in', 'search')
-('search', 'of')
-('of', 'the')
-('the', 'mythical')
-('mythical', 'treasure')
-('treasure', 'known')
-('known', 'as')
-('as', 'the')
-('the', '"One')
-('"One', 'Piece"')
-('Piece"', 'in')
-('in', 'order')
-('order', 'to')
-('to', 'become')
-('become', 'the')
-('the', 'next')
-('next', 'King')
-('King', 'of')
-('of', 'the')
-('the', 'Pirates.')
+import spacy
+
+raw_text=("In the bustling city of Techlandia, Dr. Emily Johnson, a brilliant scientist, unveiled her latest invention at the Quantum Expo. The Quantum Generator, a revolutionary device, promises to transform the way we harness energy."
+
+"As the crowd marveled at the Quantum Generator, Mark Thompson, the CEO of FutureTech Inc., approached Dr. Johnson with a partnership proposal. They discussed the potential applications of the technology in various industries, including renewable energy and telecommunications."
+
+"Meanwhile, Agent Sarah Miller of the top-secret organization CodeGuard observed the proceedings discreetly. She had received intel about a possible attempt to steal the Quantum Generator's blueprints by a group of notorious hackers known as the Binary Bandits."
+
+"During the evening gala, Dr. Johnson was awarded the prestigious Innovation Excellence Award by Mayor Jessica Rodriguez. The event was covered by the leading tech journalist, Alex Turner, who praised Dr. Johnson's contribution to scientific advancements."
+
+"However, little did they know that the Binary Bandits were already plotting their next move. In the shadows, their leader, known as Cipher, devised a plan to infiltrate FutureTech Inc. and steal the Quantum Generator's secrets."
+
+"As the night unfolded, the city of Techlandia became the stage for a high-stakes game of innovation, corporate intrigue, and espionage."
+)
+
+NER = spacy.load("en_core_web_sm",disable=["tok2vec", "tagger", "parser", "attribute_ruler", "lemmatizer"])
+
+text = NER(raw_text)
+
+for x in text.ents:
+    print(x.text,x.label_)
+
+#spacy.displacy.render(text, style="ent", jupyter=True)
+
+# spacy.explain(u"NORP")
 
 
+'''
+Output-
 
-------Trigram Model------
-('The', 'story', 'follows')
-('story', 'follows', 'the')
-('follows', 'the', 'adventures')
-('the', 'adventures', 'of')
-('adventures', 'of', 'Monkey')
-('of', 'Monkey', 'D.')
-('Monkey', 'D.', 'Luffy')
-('D.', 'Luffy', 'and')
-('Luffy', 'and', 'his')
-('and', 'his', 'crew,')
-('his', 'crew,', 'the')
-('crew,', 'the', 'Straw')
-('the', 'Straw', 'Hat')
-('Straw', 'Hat', 'Pirates,')
-('Hat', 'Pirates,', 'where')
-('Pirates,', 'where', 'he')
-('where', 'he', 'explores')
-('he', 'explores', 'the')
-('explores', 'the', 'Grand')
-('the', 'Grand', 'Line')
-('Grand', 'Line', 'in')
-('Line', 'in', 'search')
-('in', 'search', 'of')
-('search', 'of', 'the')
-('of', 'the', 'mythical')
-('the', 'mythical', 'treasure')
-('mythical', 'treasure', 'known')
-('treasure', 'known', 'as')
-('known', 'as', 'the')
-('as', 'the', '"One')
-('the', '"One', 'Piece"')
-('"One', 'Piece"', 'in')
-('Piece"', 'in', 'order')
-('in', 'order', 'to')
-('order', 'to', 'become')
-('to', 'become', 'the')
-('become', 'the', 'next')
-('the', 'next', 'King')
-('next', 'King', 'of')
-('King', 'of', 'the')
-('of', 'the', 'Pirates.')
+Techlandia GPE
+Emily Johnson PERSON
+the Quantum Expo FAC
+The Quantum Generator ORG
+the Quantum Generator ORG
+Mark Thompson PERSON
+FutureTech Inc. ORG
+Johnson PERSON
+Sarah Miller PERSON
+CodeGuard ORG
+the Quantum Generator's LAW
+the Binary Bandits LAW
+evening TIME
+Johnson PERSON
+Innovation Excellence Award ORG
+Jessica Rodriguez PERSON
+Alex Turner PERSON
+Johnson PERSON
+the Binary Bandits LAW
+Cipher PERSON
+FutureTech Inc. ORG
+the Quantum Generator's ORG
+Techlandia GPE
 
-
-
-------Trigram Model using text file------
-For sentence 1 , trigrams are: 
-('One', 'Piece', '(stylized')
-('Piece', '(stylized', 'in')
-('(stylized', 'in', 'all')
-('in', 'all', 'caps)')
-('all', 'caps)', 'is')
-('caps)', 'is', 'a')
-('is', 'a', 'Japanese')
-('a', 'Japanese', 'manga')
-('Japanese', 'manga', 'series')
-('manga', 'series', 'written')
-('series', 'written', 'and')
-('written', 'and', 'illustrated')
-('and', 'illustrated', 'by')
-('illustrated', 'by', 'Eiichiro')
-('by', 'Eiichiro', 'Oda')
-
-For sentence 2 , trigrams are: 
-('The', 'story', 'follows')
-('story', 'follows', 'the')
-('follows', 'the', 'adventures')
-('the', 'adventures', 'of')
-('adventures', 'of', 'Monkey')
-('of', 'Monkey', 'D')
-('Monkey', 'D', 'Luffy')
-('D', 'Luffy', 'and')
-('Luffy', 'and', 'his')
-('and', 'his', 'crew,')
-('his', 'crew,', 'the')
-('crew,', 'the', 'Straw')
-('the', 'Straw', 'Hat')
-('Straw', 'Hat', 'Pirates,')
-('Hat', 'Pirates,', 'where')
-('Pirates,', 'where', 'he')
-('where', 'he', 'explores')
-('he', 'explores', 'the')
-('explores', 'the', 'Grand')
-('the', 'Grand', 'Line')
-('Grand', 'Line', 'in')
-('Line', 'in', 'search')
-('in', 'search', 'of')
-('search', 'of', 'the')
-('of', 'the', 'mythical')
-('the', 'mythical', 'treasure')
-('mythical', 'treasure', 'known')
-('treasure', 'known', 'as')
-('known', 'as', 'the')
-('as', 'the', '"One')
-('the', '"One', 'Piece"')
-('"One', 'Piece"', 'in')
-('Piece"', 'in', 'order')
-('in', 'order', 'to')
-('order', 'to', 'become')
-('to', 'become', 'the')
-('become', 'the', 'next')
-('the', 'next', 'King')
-('next', 'King', 'of')
-('King', 'of', 'the')
-('of', 'the', 'Pirates')
 
 '''
